77187a4f9a0a9ddb8579d23ed30156b1dd4cef43
sangamesh kulkarni <sangameshkulkarni26@gmail.com>
2024-03-29 19:05:07 +0530

    chore: add index and remove commented code

--- application/movie_list.py
+++ application/movie_list.py
@@ -7,12 +7,15 @@ DEFAULT_OFFSET = 1
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['imdb']
+collection = db['uploads']
+collection.create_index("type")
 
 def get_movie_list(request):
     limit = int(request.args.get("limit",DEFAULT_LIMIT))
     offset = int(request.args.get("offset",DEFAULT_OFFSET))
     
-    movies = db.uploads.find().skip((offset - 1) * limit).limit(limit)
+    query = {'data.type': 'Movie'}
+    movies = collection.find(query).skip((offset - 1) * limit).limit(limit)
     movie_list = [movie['data'] for movie in movies]
     
     json_data = jsonify(movie_list)
